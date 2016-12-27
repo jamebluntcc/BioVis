4ef48b37eef23cf9f144cc4312a28bc703bc3183
chencheng <291552579@qq.com>
2016-12-27 15:55:03 +0800

    update

--- wgcna_network.R
+++ wgcna_network.R
@@ -1,7 +1,4 @@
-# #2016-12-13 wgcna plot
-#--------new way to wgcna------
-#by ggnetwork
-library(ggnetwork)
+#wgcna plot
 library(network)
 library(sna)
 library(intergraph)
@@ -9,25 +6,54 @@ library(igraph)
 library(data.table)
 library(dplyr)
 options(stringsAsFactors = F)
-wgcna_edge <- read.delim("CytoscapeInput-edges.txt",header = T)
-wgcna_node <- read.delim("CytoscapeInput-nodes.txt",header = T)
-wgcna_edge <- wgcna_edge[c(1,2,3)]
-wgcna_node <- wgcna_node[c(1,3)]
-names(wgcna_node)[2] <- c("nodeColor")
-#wgcna_edge_selected <- dplyr::filter(wgcna_edge,weight > 0.2)
-graph_data <- graph.data.frame(wgcna_edge[1:2],directed = F)
-graph_data <- simplify(graph_data)  #dorp the multigraph
-#由ig 到 network
+argv <- commandArgs(T)
+edges_data <- argv[1]
+node_data <- argv[2]
+wgcna_edges <- read.delim(edges_data,header = T)
+wgcna_nodes <- read.delim(node_data,header = T)
+wgcna_edges <- wgcna_edges[1:3]
+wgcna_nodes <- wgcna_nodes[c(1,3)]
+names(wgcna_nodes)[2] <- c("nodeColor")
+if(length(unique(wgcna_nodes$nodeColor)) <= 1){
+  stop("wgcna node color error")
+}
+#--- network function----
+net_work <- function(plotcord,social_mat,net,node.data,XD = 3.5){
+  plotcord$name <- colnames(social_mat)
+  names(node.data) <- c("nodeName","color")
+  node.data$color <- as.character(node.data$color)
+  plotcord$color <- node.data$color[match(plotcord$name,node.data$nodeName,nomatch = 0)]
+  edglist <- as.matrix.network.edgelist(net)
+  edge_plot <- data.frame(plotcord[edglist[,1],c(1,2)], plotcord[edglist[,2],c(1,2)])
+  colnames(edge_plot) <-  c("X1","Y1","X2","Y2")
+  gnet_edge <- ggplot(data=edge_plot)  + 
+    geom_segment(aes(x=X1*XD, y=Y1*XD, xend = X2*XD, yend = Y2*XD), 
+                 size = .4,colour="grey",alpha = .4)+
+    xlab("")+ylab("")+scale_x_continuous(breaks = NULL)+
+    scale_y_continuous(breaks = NULL)
+  values <- plotcord$color
+  names(values) <- values
+  gnet_point <- gnet_edge +
+    geom_point(aes(X1*XD, X2*XD,colour = color), 
+               data=plotcord,alpha = .7)+scale_color_manual(values = values)
+  return(gnet_point)
+}
+#---net work theme----
+net_theme <- theme_bw()+theme(
+  panel.background = element_blank(),
+  panel.border = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank()
+)
+theme_set(net_theme)
+# #for test
+# test_edges <- sample_n(wgcna_edges,10000)
+# test_nodes <- wgcna_nodes[wgcna_nodes$nodeName %in% c(test_edges$fromNode,test_edges$toNode),]
+graph_data <- simplify(graph.data.frame(wgcna_edges,directed = F))
 net <- asNetwork(graph_data)
-#net %v% "color" <- wgcna_node$nodeColor
-#ggnetwork transform net class to df 
-df <- ggnetwork(net,layout = "fruchtermanreingold")
-df$color <- wgcna_node$nodeColor[match(df$vertex.names,wgcna_node$nodeName,nomatch = 0)]
-write.table(df,"wgcna_data.txt",quote = F,row.names = F,sep = "\t")
-net_color <- df$color
-names(net_color) <- net_color
-network <- ggplot(df,aes(x = x, y = y, xend = xend, yend = yend))+
-  geom_edges(curvature = 0.1,size = 0.4,alpha = 0.4,color = "grey")+theme_blank()+
-  geom_point(aes(x=x,y=y,colour=color),size = 0.8)+scale_color_manual(values = net_color)
-ggsave(filename = "net_work.png",plot = network,width = 12,height = 8,type = "cairo",dpi = 300)
-
+m <- as.matrix.network.adjacency(net) 
+plotcord <- data.frame(gplot.layout.fruchtermanreingold(m,NULL))
+#---plot out---
+my_network <- net_work(plotcord,social_mat = m,net = net,node.data = test_nodes)
+ggsave(filename = "wgcna_network.png",plot = my_network,width = 8,height = 6,dpi = 300,type = "cairo")
+ggsave(filename = "wgcna_network.pdf",plot = my_network,width = 8,height = 6,dpi = 300)
