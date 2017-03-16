dd16941fb5f5883e7099c073a184d17bf9286183
jamebluntcc <jamebluntxy@gmail.com>
2017-03-16 21:53:19 +0800

    add venn and READDE.md

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# onmath_visualization
+# onmath visualiztion
+
+## venn plot
+
+venn plot 我们使用R的[VennDiagram](https://cran.r-project.org/web/packages/VennDiagram/VennDiagram.pdf)包进行可视化,设定最大可以画4个group的venn。
+
+### test data
+```r
+oneName <- function() paste(sample(LETTERS,5,replace=TRUE),collapse="")
+geneNames <- replicate(1000,oneName())
+GroupA <- sample(geneNames,400,replace = F)
+GroupB <- sample(geneNames,300,replace = F)
+GroupC <- sample(geneNames,200,replace = F)
+GroupD <- sample(geneNames,500,replace = F)
+```
+
+### test plot
+
+我们将VennDiagram封装在一个函数中,设置好一些基本的参数,当自己在实际项目中需要进行修改一些VennDiagram中的参数时,可以直接在函数中进行修改然后再保存运行。
+```
+fill_col <- c("#386cb0","#fdb462","#7fc97f","#ef3b2c")
+venn_plot(venn_data = list(A=GroupA,B=GroupB),fill_colour = fill_col[1:2],file_name = 'venn_test_group2.png')
+```
+![venn plot group=2](figure/venn_test_group2.png)
+
+```
+fill_col <- c("#386cb0","#fdb462","#7fc97f","#ef3b2c")
+venn_plot(venn_data = list(A=GroupA,B=GroupB,C=GroupC),fill_colour = fill_col[1:3],file_name = 'venn_test_group3.png')
+```
+![venn plot group=3](figure/venn_test_group3.png)
+
+```
+fill_col <- c("#386cb0","#fdb462","#7fc97f","#ef3b2c")
+venn_plot(venn_data = list(A=GroupA,B=GroupB,C=GroupC,D=GroupD),fill_colour = fill_col,file_name = 'venn_test_group4.png')
+```
+![venn plot group=2](figure/venn_test_group4.png)
+
+### VennDiagram's param
+ - file type:png
+ 
--- venn.R
+++ venn.R
@@ -1,30 +1,41 @@
-#venn plot
-#venn plot by gplots
-library(gplots)
+#----------------------------------------
+#this script to plot venn by chencheng
+#use vennDiagram library
+#create on 2017-3-15
+#----------------------------------------
+#----create test data----
+library(argparser)
 oneName <- function() paste(sample(LETTERS,5,replace=TRUE),collapse="")
 geneNames <- replicate(1000,oneName())
 GroupA <- sample(geneNames,400,replace = F)
 GroupB <- sample(geneNames,300,replace = F)
 GroupC <- sample(geneNames,200,replace = F)
 GroupD <- sample(geneNames,500,replace = F)
-input <- list(`1`=GroupA,`2`=GroupB,`3`=GroupC)
-gplots::venn(input) #no fill color
-#venn plot by vennDiagram
-library(VennDiagram)
-names(input) <- c('A','B','C')
-VennDiagram::venn.diagram(
-  x = input,filename = "test4.png",
-  col = "transparent",fill = c("cornflowerblue", "green", "yellow"), #set fill color
-  #label.col = c("orange", "white", "darkorchid4"), set number color
-  cex = 1.5,
-  fontfamily = "serif",
-  fontface = "bold",
-  #cat.col = c("darkblue", "darkgreen"), #set label color
-  cat.cex = 1.5,
-  #cat.pos = c(-10,10),  #set label pos
-  #cat.dist = 0.07,
-  cat.fontfamily = "serif",
-  rotation.degree = 0,
-  margin = 0.1  #set plot's size margin more biger the size more samll
-)
+#-----venn plot by vennDiagram-----
+if(!require(VennDiagram)){
+  install.packages("VennDiagram")
+  library(VennDiagram)
+}
+# p <- argparser::arg_parser('this is a R script to plot Venn')
+# p <- add_argument(p,'file_path',help = 'pca file path',type = "character")
+# p <- add_argument(p,'group_sample',help = 'pca group information',type = "character")
+# p <- add_argument(p,'out_path',help = 'putout pca plot path',type = "character")
+# argv <- parse_args(p)
+fill_col <- c("#386cb0","#fdb462","#7fc97f","#ef3b2c")
+venn_plot <- function(venn_data,fill_colour,file_name,main = NULL){
+  VennDiagram::venn.diagram(
+    x = venn_data,filename = file_name,main = main,
+    col = "transparent",fill = fill_colour, #set fill color
+    label.col = "white", #set number color
+    cex = 1.5,
+    fontfamily = "serif",
+    fontface = "bold",
+    cat.col = fill_colour, #set label color
+    cat.cex = 1.5,
+    cat.fontfamily = "serif",
+    rotation.degree = 0,
+    margin = 0.1  #set plot's size margin more biger the size more samll
+  )
+}
+
                           
