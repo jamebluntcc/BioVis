355653e2767d9dc1bddff45b533305e6076d3601
jamebluntcc <291552579@qq.com>
2017-06-27 17:21:07 +0800

    update venn plot

--- plot_modules/venn.R
+++ plot_modules/venn.R
@@ -1,21 +1,14 @@
-#----------------------------------------
-#this script to plot venn by chencheng
-#use vennDiagram library
-#create on 2017-3-15
-#----------------------------------------
-#----create test data----
+#---------
+#venn plot
+#---------
 library(argparser)
+library(VennDiagram)
 oneName <- function() paste(sample(LETTERS,5,replace=TRUE),collapse="")
 geneNames <- replicate(1000,oneName())
 GroupA <- sample(geneNames,400,replace = F)
 GroupB <- sample(geneNames,300,replace = F)
 GroupC <- sample(geneNames,200,replace = F)
 GroupD <- sample(geneNames,500,replace = F)
-#-----venn plot by vennDiagram-----
-if(!require(VennDiagram)){
-  install.packages("VennDiagram")
-  library(VennDiagram)
-}
 # p <- argparser::arg_parser('this is a R script to plot Venn')
 # p <- add_argument(p,'file_path',help = 'pca file path',type = "character")
 # p <- add_argument(p,'group_sample',help = 'pca group information',type = "character")
@@ -37,5 +30,3 @@ venn_plot <- function(venn_data,fill_colour,file_name,main = NULL){
     margin = 0.1  #set plot's size margin more biger the size more samll
   )
 }
-
-                          
