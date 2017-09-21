ad51547a865faec1f3ed8d0d5ba9bc12ac268fb4
jamebluntcc <291552579@qq.com>
2017-09-21 15:53:44 +0800

    add snp index manhatton diff plot

--- plot_modules/snp_index.R
+++ plot_modules/snp_index.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(stringr)
 library(tidyr)
 library(readr)
+library(gridExtra)
 library(RColorBrewer)
 #-- define func--
 deal_snp_cell <- function(cell,split = ','){
@@ -20,7 +21,7 @@ deal_snp_cell <- function(cell,split = ','){
   }
 }
 
-tidy_snp_data <- function(data,fileName){
+tidy_snp_data <- function(data,fileName='snp_tidy.txt'){
   names(data) <- c('V1','V2','V3','V4')
   df_3 <- as.data.frame(t(sapply(data$V3,deal_snp_cell)))
   df_4 <- as.data.frame(t(sapply(data$V4,deal_snp_cell)))
@@ -28,13 +29,14 @@ tidy_snp_data <- function(data,fileName){
   data$V6 <- df_3$V2
   data$V7 <- df_4$V1
   data$V8 <- df_4$V2
-  data <- dplyr::filter(snp_hq_tidy,(V6 / (V5+V6) >= 0.3 & (V5+V6) >= 5) |
-                                    (V8 / (V7+V8) >= 0.3 & (V7+V8) >= 5))
+  data <- dplyr::filter(data,(V6 / (V5+V6) >= 0.3 & (V5+V6) >= 5) |
+                                   (V8 / (V7+V8) >= 0.3 & (V7+V8) >= 5))
+  data <- data[,c(1,2,5,6,7,8)]
   write.table(data,fileName,quote = F,row.names = F,sep = "\t")
 }
 
 tidy_sliderWindow_data <- function(data,header){
-  names(data) <- paste0('X',1:dim(data)[1])
+  names(data) <- paste0('X',1:dim(data)[2])
   data <- mutate(na.omit(data),X9=(X6)/(X5+X6),X10=X8/(X7+X8))
   data <- dplyr::filter(data,X4 > 10)
   data <- data[,c(1,2,9,10)]
@@ -57,17 +59,16 @@ theme_set(my_theme)
 
 merge_plot <- function(data,filename,
                        col=brewer.pal(3,'Set2'),
-                       labels,
                        break_seq,
-                       label_seq){
+                       label_seq,
+                       labels){
   names(data) <- c('chr','pos','V1','V2','V3')
-  data$chr <- paste0('chr',data$chr)
-  text_df <- data.frame(x=7e7,y=0.85)
+  # data$chr <- paste0('chr',data$chr)
   chr_set <- unique(data$chr)
   for(i in chr_set){
     up_p <- ggplot(data[which(data$chr==i),],aes(x=pos,y=V1))+
       geom_point(color=col[1],size=rel(0.8))+
-      geom_text(data = text_df,aes(x=x,y=y,label=labels[1]),size=8)+
+      annotate('text',x=1.2e8,y=0.85,label=labels[1],size=6,parse=T)+
       geom_smooth(se=F,color='red')+
       xlab("Postion")+ylab("SNP Index")+
       scale_x_continuous(breaks = break_seq,
@@ -77,7 +78,7 @@ merge_plot <- function(data,filename,
       guides(color=guide_legend(title = ""))+ggtitle(label = i)
     md_p <- ggplot(data[which(data$chr==i),],aes(x=pos,y=V2))+
       geom_point(color=col[2],size=rel(0.8))+
-      geom_text(data = text_df,aes(x=x,y=y,label=labels[2]),size=8)+
+      annotate('text',x=1.2e8,y=0.85,label=labels[2],size=6,parse=T)+
       geom_smooth(se=F,color='red')+
       xlab("Postion")+ylab("SNP Index")+
       scale_x_continuous(breaks = break_seq,
@@ -88,8 +89,8 @@ merge_plot <- function(data,filename,
     do_p <- ggplot(data[which(data$chr==i),],aes(x=pos,y=V3))+
       geom_point(color=col[3],size=rel(0.8))+
       geom_smooth(se=F,color='red')+
-      geom_text(data = text_df,aes(x=x,y=y,label=labels[3]),size=8)+
-      xlab("Postion")+ylab("DIFF")+
+      annotate('text',x=1.2e8,y=0.85,label="paste(Delta,snp-index)",size=6,parse=T)+
+      xlab("Postion")+ylab(expression(paste(Delta,'snp-index')))+
       scale_x_continuous(breaks = break_seq,
                          labels = paste0(label_seq,'M'))+
       scale_y_continuous(breaks = seq(from=-1,to=1,by=0.5),
@@ -100,3 +101,47 @@ merge_plot <- function(data,filename,
     ggsave(filename = paste0(filename,i,'.png'),plot = merge_p,width = 8,height = 12)
   }
 }
+
+plot_manhatton_diff_snp_index <- function(data,filename='snp_diff_manhatton',w=12,h=8){
+  plot_data <- data
+  plot_data$cumpos <- 0
+  plot_data$col <- ''
+  each_chr_max_pos <- plot_data %>% group_by(Chrom) %>% summarise(max_chr=max(Pos))
+  chr_sets <- unique(plot_data$Chrom)
+  snp_plot_col <- rep(brewer.pal(9,'Set1'),5)[1:length(chr_sets)]
+  add_chr_num <- cumsum(each_chr_max_pos$max_chr)
+  max_len <- ceiling(add_chr_num[length(add_chr_num)] * 1.2)
+  for(i in 2:length(chr_sets)){
+    plot_data$cumpos[which(plot_data$Chrom == chr_sets[i])] <- plot_data$Pos[which(plot_data$Chrom == chr_sets[i])]+add_chr_num[i-1]
+  }
+  plot_data$cumpos[which(plot_data$Chrom == chr_sets[1])] <- plot_data$Pos[which(plot_data$Chrom == chr_sets[1])]
+  for(i in 1:length(chr_sets)){
+    plot_data$col[which(plot_data$Chrom == chr_sets[i])] <- snp_plot_col[i]
+  }
+  names(snp_plot_col) <- snp_plot_col
+  gg <- ggplot(plot_data,aes(x=cumpos,y=DIFF,color=col))+
+    geom_point()+
+    scale_x_continuous(breaks = seq(from=0,to=max_len,length.out = length(chr_sets)),
+                       labels = chr_sets)+
+    scale_color_manual(values = snp_plot_col)+
+    guides(color=F)+xlab("Postion")+ylab(expression(paste(Delta,'snp-index')))+
+    ylim(c(-1,1))+theme(panel.grid.minor = element_blank())
+  ggsave(paste(filename,'png',sep = "."),plot = gg,width = w,height = h)
+  ggsave(paste(filename,'pdf',sep = "."),plot = gg,width = w,height = h)
+}
+#-- read file and tidy--
+snp_data <- read_delim('data/mRNA.ZYKY.hq.snp.ann.merge.table',col_names = T,delim = "\t")
+snp_data_tidy <- dplyr::select(snp_data,c('Chrom','Pos','M','W'))
+snp_data_tidy <- tidy_snp_data(snp_data_tidy)
+snp_sw_data <- read_delim("snp_tidy.bed3.out",delim = "\t",col_names = F)
+tidy_sw_data <- tidy_sliderWindow_data(snp_sw_data,header = c("Chrom","Pos","M","W"))
+tidy_sw_data <- dplyr::mutate(tidy_sw_data,DIFF=M-W)
+
+library(gridExtra)
+merge_plot(tidy_sw_data,
+           filename = "snp_MvsW_",
+           break_seq = seq(from=0,to=8e8,by=2e8),
+           label_seq = seq(from=0,to=800,by=200),
+           labels = c("italic(M)","italic(W)"))
+
+plot_manhatton_diff_snp_index(tidy_sw_data,filename = "snpindex_MvsW_manhatton")
